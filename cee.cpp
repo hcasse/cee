065c883826436f8edd6522833a7d3db71885a5b9
Hugues Cassé <hugues.casse@laposte.net>
2018-04-05 14:38:12 +0200

    Added simple data cache analysis. (PID analysis is coming).

--- cee.cpp
+++ cee.cpp
@@ -1,5 +1,6 @@
 #include <otawa/app/Application.h>
 #include <otawa/cache/cat2/features.h>
+#include <otawa/dcache/features.h>
 #include <otawa/cfg/features.h>
 #include <otawa/hard/CacheConfiguration.h>
 
@@ -9,21 +10,17 @@ using namespace otawa;
 class CEE: public Application {
 public:
 	CEE(void): Application("CEE", Version(1, 0, 0)),
-	quiet(option::SwitchOption::Make(*this).cmd("-q").cmd("--quiet").description("Do not display header line"))
+	quiet(option::SwitchOption::Make(*this).cmd("-q").cmd("--quiet").description("Do not display header line")),
+	dcache(option::SwitchOption::Make(*this).cmd("-d").cmd("--dcache").description("Perform simple data cache analysis"))
 	{
 	}
 	
 protected:
-	void work(const string& task, PropList& props) throw(elm::Exception) {
-		CACHE_CONFIG_PATH(props) = "cache.xml";
 
+	void performICacheAnalysis(void) {
 		// launch instruction cache analysis
 		require(ICACHE_ACS_MAY_FEATURE);
 		require(ICACHE_CATEGORY2_FEATURE);
-
-		// get the number of sets
-		const hard::CacheConfiguration *cconf = hard::CACHE_CONFIGURATION(workspace());
-		const hard::Cache *icache = cconf->instCache();
 
 		// compute statistics
 		t::uint64
@@ -48,15 +45,6 @@ protected:
 						cnt++;
 					}
 			}
-		if(!quiet)
-			cout
-				<< "   Total"
-				<< "      AH"
-				<< "      AM"
-				<< "      PE"
-				<< "      NC"
-				<< " Benchmark"
-				<< io::endl;
 		cout
 			<< io::width(8, cnt).right()
 			<< io::width(8, ah).right()
@@ -67,8 +55,70 @@ protected:
 			<< io::endl;
 	}
 
+	void performDCacheAnalysis(void) {
+		// TODO fix list of features in OTAWA documentation
+		
+		// data cache analysis
+		require(dcache::CLP_BLOCK_FEATURE);
+		require(dcache::MAY_ACS_FEATURE);
+		require(dcache::CATEGORY_FEATURE);
+		
+		// compute statistics
+		// compute statistics
+		t::uint64
+			cnt = 0,
+			ah = 0,
+			am = 0,
+			pe = 0,
+			nc = 0;
+		const CFGCollection& coll = **INVOLVED_CFGS(workspace());
+		for(int i = 0; i < coll.count(); i++)
+			for(CFG::BBIterator bb(coll.get(i)); bb; bb++) {
+				Pair<int, dcache::BlockAccess *> tab = dcache::DATA_BLOCKS(bb);
+				for(int i = 0; i < tab.fst; i++) {
+					switch(dcache::CATEGORY(tab.snd[i])) {
+					case ALWAYS_HIT:		ah++; break;
+					case ALWAYS_MISS:		am++; break;
+					case FIRST_MISS:		pe++; break;
+					case NOT_CLASSIFIED:	nc++; break;
+					default:				ASSERT(false); break;
+					}
+					cnt++;
+				}
+			}
+		cout
+			<< io::width(8, cnt).right()
+			<< io::width(8, ah).right()
+			<< io::width(8, am).right()
+			<< io::width(8, pe).right()
+			<< io::width(8, nc).right()
+			<< ' ' << workspace()->process()->program()->name()
+			<< io::endl;
+	}
+
+	void work(const string& task, PropList& props) throw(elm::Exception) {
+		require(VIRTUALIZED_CFG_FEATURE);
+		CACHE_CONFIG_PATH(props) = "cache.xml";
+
+		if(!quiet)
+			cout
+				<< "   Total"
+				<< "      AH"
+				<< "      AM"
+				<< "      PE"
+				<< "      NC"
+				<< " Benchmark"
+				<< io::endl;
+		
+		performICacheAnalysis();
+		
+		if(dcache)
+			performDCacheAnalysis();
+	}
+
 private:
 	option::SwitchOption quiet;
+	option::SwitchOption dcache;
 };
 
 OTAWA_RUN(CEE);
